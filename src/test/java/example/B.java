bd912997007e76a83eae5a0227808fec2a347fe7
r12mbo <48469030+r12mbo@users.noreply.github.com>
2019-03-15 05:53:28 +0530

    p

---
+++ src/test/java/example/B.java
@@ -0,0 +1,32 @@
+package example;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class B {
+	static 
+	{
+		String key = "webdriver.chrome.driver";
+		String value = "./driver/chromedriver.exe";	
+		System.setProperty(key, value);
+	}
+	static 
+	{
+		String key = "webdriver.gecko.driver";
+		String value = "./driver/geckodriver.exe";	
+		System.setProperty(key, value);
+	}
+	public static void main(String[] args)   {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/akshay/Desktop/Demo.html");
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		System.out.println(elements);
+		System.out.println("Hi");
+	
+
+	}
+}
